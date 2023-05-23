9f09523e669fd65bcf05d4b19cc9702aa05425cb
Peter Njiru <peternjirum@gmail.com>
2023-05-22 20:31:04 -0400

    completed props basics lab

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,14 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+// import user from "../data/user";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name="Liza" city="New York" color="firebrick"/>
+      <About bio="I made this!"/>
     </div>
   );
 }
